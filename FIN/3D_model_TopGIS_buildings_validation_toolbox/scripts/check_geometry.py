bace3843692690162552a0f0c8a5ef782996a5a9
Jan Horák <horak.jan@kambrno.cz>
2024-01-18 10:45:29 +0100

    change

--- FIN/3D_model_TopGIS_buildings_validation_toolbox/scripts/check_geometry.py
+++ FIN/3D_model_TopGIS_buildings_validation_toolbox/scripts/check_geometry.py
@@ -1,3 +1,7 @@
+from toolbox_utils.clear_selection import clear_selection
+from toolbox_utils.gdb_getter import get_gdb_path_3D_geoms, get_gdb_path_3D_geoms_multiple
+# log_it printuje jak do arcgis console tak do souboru
+from toolbox_utils.messages_print import aprint, log_it, setup_logging
 import os
 import sys
 import json
@@ -5,9 +9,6 @@ import arcpy
 import logging
 from typing import (List, Union)
 numeric = Union[int, float]
-from toolbox_utils.messages_print import aprint, log_it, setup_logging # log_it printuje jak do arcgis console tak do souboru
-from toolbox_utils.gdb_getter import get_gdb_path_3D_geoms, get_gdb_path_3D_geoms_multiple
-from toolbox_utils.clear_selection import clear_selection
 
 
 class CheckGeometry(object):
@@ -34,7 +35,6 @@ class CheckGeometry(object):
             enabled='True',
         )
 
-
         root_dir_lokalita_multiple = arcpy.Parameter(
             name="in_dir_multiple",
             displayName="Path location of root folder (Lokalita_00_YYYY_MM_DD)",
@@ -53,20 +53,19 @@ class CheckGeometry(object):
             parameterType='Optional',
             enabled='True',
         )
-        
 
         tolerance.value = 0
         tolerance.filter.type = "Range"
         tolerance.filter.list = [0, 500]
-        log_file_path.value = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(arcpy.mp.ArcGISProject("CURRENT").filePath))), 'logs')
+        log_file_path.value = os.path.join(os.path.dirname(os.path.dirname(
+            os.path.dirname(arcpy.mp.ArcGISProject("CURRENT").filePath))), 'logs')
 
         params = [log_file_path, root_dir_lokalita_multiple, tolerance]
 
         return params
 
-    
     def isLicensed(self):
-        # TODO - ?? 
+        # TODO - ??
         return True
 
     def updateParameters(self, parameters):
@@ -84,23 +83,25 @@ class CheckGeometry(object):
         """Process the parameters"""
         param_values = (p.valueAsText for p in parameters)
         try:
-            main(*param_values) 
+            main(*param_values)
         except Exception as err:
             arcpy.AddError(err)
             sys.exit(1)
         return
+
 
 def init_logging(log_dir_path: str) -> None:
     '''
     initializes logging instance (logger object, format, log_file locatio etc.) for current tool
     '''
     class_name = CheckGeometry().name.replace(' ', '_')
-    setup_logging(log_dir_path,class_name, __name__)
+    setup_logging(log_dir_path, class_name, __name__)
 
 
 def fieldExists(dataset: str, field_name: str) -> bool:
     """Return boolean indicating if field exists in the specified dataset."""
     return field_name in [field.name for field in arcpy.ListFields(dataset)]
+
 
 def tableExists(table_name: str) -> bool:
     '''Return boolean indicating if table exists in the specified workspace.'''
@@ -115,7 +116,8 @@ def get_fc_from_gdb_within_dataset(gdb_path: str, geometry='') -> str:
 
     for dat in arcpy.ListDatasets():
         for fc in arcpy.ListFeatureClasses(feature_type=geometry, feature_dataset=dat):
-            return fc    
+            return fc
+
 
 def get_fc_from_gdb_direct(gdb_path: str, fc_name=None) -> str:
     '''
@@ -126,15 +128,16 @@ def get_fc_from_gdb_direct(gdb_path: str, fc_name=None) -> str:
     for fc in arcpy.ListFeatureClasses(fc_name):
         return fc
 
+
 def no_geometry(fc_geometry_object: object, plocha_id: int) -> int:
     '''
     Returns list of ID_PLO where feature has no geometry.
     '''
     if fc_geometry_object == None:
-        return plocha_id 
+        return plocha_id
 
 
-# TODO - muzem po nich chtit planaritu? 
+# TODO - muzem po nich chtit planaritu?
 # def check_if_planar()
 #     return
 
@@ -142,15 +145,17 @@ def no_geometry(fc_geometry_object: object, plocha_id: int) -> int:
 def check_id_plo_attr_against_geometry(fc_geometry_object: object, plocha_kod: str, plocha_id: int, tolerance: int, plocha_kod_spec: int = None) -> int:
     if plocha_kod == plocha_kod_spec:
         for part in fc_geometry_object:
-                z_vals = [pnt.Z for pnt in part if pnt]
-                if plocha_kod_spec == 3 or plocha_kod_spec == 1:
-                    if len(set(z_vals)) == 1:
-                        return plocha_id
-                elif plocha_kod_spec == 2 or plocha_kod_spec == 4:
-                    if len(set(z_vals)) != 1 and (max(z_vals) - min(z_vals)) > tolerance:
-                        return plocha_id
-                else:
-                    log_it('Input parameters were specified incorrectly', 'warning', __name__)
+            z_vals = [pnt.Z for pnt in part if pnt]
+            if plocha_kod_spec == 3 or plocha_kod_spec == 1:
+                if len(set(z_vals)) == 1:
+                    return plocha_id
+            elif plocha_kod_spec == 2 or plocha_kod_spec == 4:
+                if len(set(z_vals)) != 1 and (max(z_vals) - min(z_vals)) > tolerance:
+                    return plocha_id
+            else:
+                log_it('Input parameters were specified incorrectly',
+                       'warning', __name__)
+
 
 def check_parts_in(input_fc: str, level=None) -> List:
     '''
@@ -161,9 +166,8 @@ def check_parts_in(input_fc: str, level=None) -> List:
     parent_object = {}
     with arcpy.da.SearchCursor(input_fc, [level, 'PLOCHA_KOD']) as cur:
         for row in cur:
-            # create hashtable with SEG_ID/RUIAN_IBO: [*PLOCHA_KOD]  
-                parent_object.setdefault(int(row[0]), []).append(int(row[1]))
-    
+            # create hashtable with SEG_ID/RUIAN_IBO: [*PLOCHA_KOD]
+            parent_object.setdefault(int(row[0]), []).append(int(row[1]))
 
     ids = []
 
@@ -183,7 +187,7 @@ def build_stats(input_fc: str, tolerance) -> None:
     Runtime function for chekcing geometry conditions
     '''
 
-    log_it(f'build_stats ran {input_fc}, {tolerance}', 'info', __name__)
+    log_it(f'Lokalita: {input_fc}',  'info', __name__)
 
     stats = {}
     plocha_kod_types = {
@@ -198,41 +202,47 @@ def build_stats(input_fc: str, tolerance) -> None:
 
             geometry_object = row[1]
             plocha_kod = int(row[2])
-            plocha_id = int(row[3])    
+            plocha_id = int(row[3])
             # log_it(f'build_stats looping {input_fc}, geom_obj: {geometry_object}, plocha_id {plocha_id}', 'info', __name__)
 
             no_geom_ids = no_geometry(geometry_object, plocha_id)
             if no_geom_ids:
-                stats.setdefault('plochy_bez_geometrie', []).append(no_geom_ids)
+                stats.setdefault('plochy_bez_geometrie',
+                                 []).append(no_geom_ids)
 
             for k, v in plocha_kod_types.items():
-                curr_invalid_id_plo = check_id_plo_attr_against_geometry(geometry_object, plocha_kod, plocha_id, tolerance=tolerance, plocha_kod_spec=v)
+                curr_invalid_id_plo = check_id_plo_attr_against_geometry(
+                    geometry_object, plocha_kod, plocha_id, tolerance=tolerance, plocha_kod_spec=v)
                 if curr_invalid_id_plo:
-                    stats.setdefault(f'attribut_{k}_se_neshoduje_s_geometrii_ploch_ID-PLO', []).append(curr_invalid_id_plo)
-    
-    log_it(f'check_id_plo_attr_finished {input_fc}, {tolerance}', 'info', __name__)
+                    stats.setdefault(
+                        f'attribut_{k}_se_neshoduje_s_geometrii_ploch_ID-PLO', []).append(curr_invalid_id_plo)
 
+    log_it(
+        f'check_id_plo_attr_finished {input_fc}, {tolerance}', 'info', __name__)
 
-    stats['segmenty_bez_nebo_s_vice_nezli_jednim_polygonem_pro_plochu_zakladova_deska_ID-SEG'] = check_parts_in(input_fc, level='ID_SEG')
-    stats['budovy_bez_stresni_plochy_RUIAN-IBO'] = check_parts_in(input_fc, level='RUIAN_IBO')
-    
+    stats['segmenty_bez_nebo_s_vice_nezli_jednim_polygonem_pro_plochu_zakladova_deska_ID-SEG'] = check_parts_in(
+        input_fc, level='ID_SEG')
+    stats['budovy_bez_stresni_plochy_RUIAN-IBO'] = check_parts_in(
+        input_fc, level='RUIAN_IBO')
+
     return stats
 
 
-def log_out_stats(stats: dict, fc_name:str)-> None:
+def log_out_stats(stats: dict, fc_name: str) -> None:
     '''
     Logs out ids of invalid features. 
     '''
     i = 0
-    for k,v in stats.items():
+    for k, v in stats.items():
         if v:
             i += 1
-            log_it(f'{k.replace("_", " ").replace("-", "_")}: {v}', 'warning', __name__)
+            log_it(f'{k.replace("_", " ").replace("-", "_")}: {v}',
+                   'warning', __name__)
     if i == 0:
-        log_it(f'{fc_name} geometry is correct', 'info', __name__)     
+        log_it(f'{fc_name} geometry is correct', 'info', __name__)
 
 
-def main(log_dir_path: str, location_root_folder_paths: str, tolerance:int = 0) -> None:
+def main(log_dir_path: str, location_root_folder_paths: str, tolerance: int = 0) -> None:
     '''
     Main runtime.
     '''
@@ -240,22 +250,20 @@ def main(log_dir_path: str, location_root_folder_paths: str, tolerance:int = 0) 
     init_logging(log_dir_path)
 
     geoms = ['PolygonZ', 'Multipatch']
-    
 
     for location_folder in location_root_folder_paths.split(';'):
-        log_it('-' * 15 , 'info', __name__)
+        log_it('-' * 15, 'info', __name__)
         log_it(f'Checking {location_folder}', 'info', __name__)
-        polygonZgdb = get_gdb_path_3D_geoms(location_folder, geoms[0]) 
+        polygonZgdb = get_gdb_path_3D_geoms(location_folder, geoms[0])
         cur_fc = get_fc_from_gdb_within_dataset(polygonZgdb)
         clear_selection(cur_fc)
-        log_out_stats(build_stats(cur_fc, float(tolerance)),cur_fc) 
-
+        log_out_stats(build_stats(cur_fc, float(tolerance)), cur_fc)
 
 
 ###################################################
 ############# Run the tool from IDE ###############
 
-## fake parametry
+# fake parametry
 # location_root_folder_paths = fr'I:\04_Hall_of_Fame\11_Honza_H\00_Projekty\12_3D_model_validation_refactoring\02_Input_Data\TopGIS_clean\Lokalita_43_2022_08_11'
 # location_root_folder_paths_broken = fr'I:\04_Hall_of_Fame\11_Honza_H\00_Projekty\12_3D_model_validation_refactoring\02_Input_Data\Broken_for_testing\PolygonZ_lokality_root_folders_broken\Lokalita_45_2022_08_11'
 # log_file_path = r'I:\04_Hall_of_Fame\11_Honza_H\00_Projekty\12_3D_model_validation_refactoring\01_Developement\02_Output\test_logs'  # bude parameter
